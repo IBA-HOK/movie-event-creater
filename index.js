e9cae4ea20f9a14311f3b2bddf7b714e76fff748
kometsubu44 <63544311+a-001@users.noreply.github.com>
2022-12-14 09:39:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
+const fs=require("fs")
 const extractFrame = require('ffmpeg-extract-frame')
+fs.rmSync("images", { recursive: true });
 try{fs.mkdirSync('images')}catch(e){}
 for (var i = 0; i < 100; i++) {
 var n = "00000"+i.toString()
